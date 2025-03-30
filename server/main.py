16b5107aea6ea03dfc5bce90296b22fd537a086e
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2025-03-30 22:20:29 +0900

    chore(deps): freeze virtual environment package dependencies

--- server/main.py
+++ server/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
 from database import SessionLocal
+
+# from handlers.restaurant import recommendRestaurant
 from models.restaurant import Restaurant
 
 app = FastAPI()
@@ -8,7 +10,10 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
+    """server health check"""
     db = SessionLocal()
     restaurant = db.query(Restaurant).filter(Restaurant.id == 1).first()
+
+    # recommended_restaurants = recommendRestaurant()
 
     return {"message": restaurant.business_name}
